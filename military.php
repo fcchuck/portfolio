0d5adb17bbea7ce484c7a065eb889944379264cf
fcchuck <fcchuck2@gmail.com>
2015-08-07 11:44:48 -0400

    Adding pages

---
+++ military.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Charles' Portfolio</title>
+        <!--Next line is a link to the Google font used-->
+		<link href='http://fonts.googleapis.com/css?family=Orbitron:400,500' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<!--next two lines link to the jquery file.  Added so as to use Modals-->
+		<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </head>
+	
+    <body>
+
+
+<?php
+$active = "navbar navbar-inverse navbar-fixed-top";
+include ("includes/nav-menu.php);
+?>
+
+</body>
+
+</html>
---
+++ nav-menu.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!--container-fluid will make the navbar the size of the screen as opposed to container making it the size of the page-->
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--*************Brand is the button to return to the Portfolio homepage***If user will be sent to other pages.  If other pages are imbedded then it will only be a logo for decoratiove purposes-->
+                <a class="navbar-brand" href="index.html">Brand</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <!--link to open default email to send me an email directly-->
+                    <li class="active"><a href="mailto:fcchuck2@gmail.com">Email Charles</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Information <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="images/Earl C Nelson Resume.htm" target="_blank">Resume</a></li>
+                            <li><a href="https://www.linkedin.com/pub/charles-nelson/27/512/9a1" target="_blank">LinkedIn</a></li>
+                            <li><a href="https://www.facebook.com/charles.nelson.9404/about" target="_blank">Facebook</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <!--search code is commented out unless I decide to add seach parameters-->
+               <!--<form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>-->
+                <ul class="nav navbar-nav navbar-center">
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Online Presence <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="images/Earl C Nelson Resume.htm" target="_blank">Resume</a></li>
+                            <li><a href="https://www.linkedin.com/pub/charles-nelson/27/512/9a1" target="_blank">LinkedIn</a></li>
+                            <li><a href="https://www.facebook.com/charles.nelson.9404/about" target="_blank">Facebook</a></li>>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav><!--End of Navigation Bar -->
